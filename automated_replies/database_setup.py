ddbb5e7b0c72442b2be183f4b884ddf78fac1057
Matthew Tartt <87999009+Tartticus@users.noreply.github.com>
2025-06-18 02:45:31 -0700

    Create database_setup.py

---
+++ automated_replies/database_setup.py
@@ -0,0 +1,10 @@
+
+import duckdb
+
+conn = duckdb.connect('Tweets.duckdb')
+conn.execute("CREATE SEQUENCE seq START 1;")
+conn.execute('''Create Table If Not Exists Tweet_Replies (
+    id INTEGER DEFAULT nextval('seq'),
+    DateTime DateTime,
+    Tweet TEXT Primary Key,
+    Reply TEXT)''')
